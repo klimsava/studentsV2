ee5357d2770b3a618774e53efff1163abae22c63
Klim <klimsava96@mail.ru>
2020-11-24 00:16:08 +0300

    Update studentsDB
    Update studentsDB

--- db/students_db.sql
+++ db/students_db.sql
@@ -3,12 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 19, 2020 at 10:58 AM
+-- Generation Time: Nov 23, 2020 at 09:06 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `students_db`
@@ -21,10 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `courses` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `time` time NOT NULL
+  `time` time DEFAULT '12:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -32,9 +38,9 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `name`, `description`, `time`) VALUES
-(1, 'Java Script', 'Courses Js', '20:15:00'),
-(4, 'PHP', 'Courses PHP', '11:00:00'),
-(23, 'Test', 'tessssssssssst', '12:15:00');
+(2, 'PHP', 'Training in the programming language PHP', '11:35:00'),
+(4, 'Java Script', 'Training in the programming language JS.', '12:30:00'),
+(5, 'MySQL', 'Learning and working with the database MySQL', '14:45:00');
 
 -- --------------------------------------------------------
 
@@ -43,10 +49,10 @@ INSERT INTO `courses` (`id`, `name`, `description`, `time`) VALUES
 --
 
 CREATE TABLE `students` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `first_name` varchar(32) NOT NULL,
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL
+  `age` int(11) NOT NULL DEFAULT '18'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -54,9 +60,30 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `first_name`, `last_name`, `age`) VALUES
-(1, 'Klim', 'Savanovich', 24),
-(5, 'Adam', 'Smith', 44),
-(37, 'Max', 'Owner', 23);
+(11, 'Klim', 'Savamovich', 24),
+(12, 'Max', 'Owner', 13),
+(13, 'adam', 'Smith', 50),
+(20, 'test', 'test', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_courses`
+--
+
+CREATE TABLE `student_courses` (
+  `student_id` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_courses`
+--
+
+INSERT INTO `student_courses` (`student_id`, `course_id`) VALUES
+(13, 5),
+(12, 5),
+(12, 4);
 
 --
 -- Indexes for dumped tables
@@ -74,7 +101,15 @@ ALTER TABLE `courses`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `first_name` (`first_name`,`last_name`);
+  ADD UNIQUE KEY `first_name` (`first_name`),
+  ADD UNIQUE KEY `last_name` (`last_name`);
+
+--
+-- Indexes for table `student_courses`
+--
+ALTER TABLE `student_courses`
+  ADD KEY `stud_ind` (`student_id`),
+  ADD KEY `cour_ind` (`course_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -84,10 +119,25 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `student_courses`
+--
+ALTER TABLE `student_courses`
+  ADD CONSTRAINT `student_courses_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `student_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
